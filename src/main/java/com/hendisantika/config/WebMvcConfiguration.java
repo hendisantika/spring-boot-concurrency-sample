adfe2c86082c984b2e9b4f9af4e0522d4e1c3d27
Hendi Santika <hendisantika@gmail.com>
2022-04-17 09:32:22 +0700

    #18 - Add addResourceHandlers in WebMvcConfiguration

--- src/main/java/com/hendisantika/config/WebMvcConfiguration.java
+++ src/main/java/com/hendisantika/config/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.hendisantika.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -18,4 +19,10 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
             "classpath:/META-INF/resources/", "classpath:/resources/",
             "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
 }
