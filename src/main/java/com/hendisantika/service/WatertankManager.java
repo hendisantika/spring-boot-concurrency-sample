780673f5bcc373adc91c874c59f1bad9a61a1ac8
Hendi Santika <hendisantika@gmail.com>
2022-04-17 09:35:31 +0700

    #27 - Add createALeak in WatertankManager

--- src/main/java/com/hendisantika/service/WatertankManager.java
+++ src/main/java/com/hendisantika/service/WatertankManager.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by IntelliJ IDEA.
@@ -82,6 +85,38 @@ public class WatertankManager {
             return true;
         }
         return false;
+
+    }
+
+    /**
+     * Method will execute new {@link Runnable} in order to create a water leak on a
+     * specific water-tank
+     *
+     * @param literOfLeak - the amount of liters to subtract from the water-tank
+     *                    (The leak)
+     * @param everySec    - the amount of time to wait between the {@link Runnable}
+     *                    execution
+     * @param watertankId - the water-tank id
+     */
+    public void createALeak(double literOfLeak, long everySec, Integer watertankId) {
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                log.info("New leak of {} liters was created for watertank# {}", literOfLeak, watertankId);
+                Watertank watertank = watertankServiceStub.getWatertankById(watertankId);
+                // In case the amount of lean to subtract is not bigger the current amount of
+                // water left in the water - tank
+                if (watertank.getCurrentCapacity() > literOfLeak) {
+                    watertank.setCurrentCapacity(watertank.getCurrentCapacity() - literOfLeak);
+                } else {
+                    // In case the amount of lean to subtract is bigger - just empty the whole water
+                    // - tank
+                    watertank.setCurrentCapacity(0);
+                }
+
+            }
+        }, everySec, everySec, TimeUnit.SECONDS);
 
     }
 
