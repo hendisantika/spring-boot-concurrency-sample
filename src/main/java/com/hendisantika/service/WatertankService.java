6cdabf394b9ce618b82ae1fe6f9128378774df0d
Hendi Santika <hendisantika@gmail.com>
2022-04-17 09:30:13 +0700

    #15 - Add WatertankService

---
+++ src/main/java/com/hendisantika/service/WatertankService.java
@@ -0,0 +1,18 @@
+package com.hendisantika.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-concurrency-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/04/22
+ * Time: 09.29
+ */
+@Service
+@Slf4j
+public class WatertankService {
+}
