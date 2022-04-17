d440ea2bab298c9d3de2d6e6a53e4b5f50bfa971
Hendi Santika <hendisantika@gmail.com>
2022-04-17 09:28:31 +0700

    #11 - Add WatertankHttpClientUtils

---
+++ src/main/java/com/hendisantika/util/WatertankHttpClientUtils.java
@@ -0,0 +1,17 @@
+package com.hendisantika.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-concurrency-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/04/22
+ * Time: 09.28
+ */
+@Component
+@SuppressWarnings("rawtypes")
+public class WatertankHttpClientUtils {
+}
