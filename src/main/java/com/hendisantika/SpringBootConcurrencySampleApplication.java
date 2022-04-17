64f1169c5e6b050f8a2105b5033c37ecb31d383f
Hendi Santika <hendisantika@gmail.com>
2022-04-17 09:32:05 +0700

    #17 - Add WebMvcConfiguration

--- src/main/java/com/hendisantika/SpringBootConcurrencySampleApplication.java
+++ src/main/java/com/hendisantika/SpringBootConcurrencySampleApplication.java
@@ -1,9 +1,15 @@
 package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
+@ServletComponentScan
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class SpringBootConcurrencySampleApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/hendisantika/config/WebMvcConfiguration.java
@@ -0,0 +1,21 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-concurrency-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/04/22
+ * Time: 09.31
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+}
