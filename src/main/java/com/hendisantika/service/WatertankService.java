f945a29ce45cb4db2a6b558f435aed3112f7a729
Hendi Santika <hendisantika@gmail.com>
2022-04-17 09:36:50 +0700

    #30 - Add getAllWatertanks in  WatertankService

--- src/main/java/com/hendisantika/service/WatertankService.java
+++ src/main/java/com/hendisantika/service/WatertankService.java
@@ -1,11 +1,14 @@
 package com.hendisantika.service;
 
 import com.hendisantika.config.WatertankInMemoryDB;
+import com.hendisantika.model.Watertank;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,12 @@ public class WatertankService {
      */
     @Autowired
     private WatertankInMemoryDB watertankInMemoryDB;
+
+    /**
+     * Get all Cached waterTanks
+     */
+    public Map<Integer, Watertank> getAllWatertanks() {
+        return watertankInMemoryDB.getWatertanks();
+    }
 
 }
